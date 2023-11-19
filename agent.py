28c90be2072faf5816b4e334aa7843c4ba6fc460
hilmario <hei028@uit.no>
2023-11-19 19:19:54 +0100

    Check Agent Class for PyTorch Compatibility
    Reviewed Agent class in agent.py.  I think there is no changes needed for PyTorch compatibility.

--- agent.py
+++ agent.py
@@ -1,4 +1,7 @@
 import torch
+import numpy as np
+import pickle
+from replay_buffer import ReplayBufferNumpy
 
 def huber_loss(y_true, y_pred, delta=1):
     """
@@ -17,3 +20,199 @@ def huber_loss(y_true, y_pred, delta=1):
 
     return torch.where(is_small_error, squared_loss, linear_loss).mean()
 
+class Agent():
+    """Base class for all agents
+    This class extends to the following classes
+    DeepQLearningAgent
+    HamiltonianCycleAgent
+    BreadthFirstSearchAgent
+
+    Attributes
+    ----------
+    _board_size : int
+        Size of board, keep greater than 6 for useful learning
+        should be the same as the env board size
+    _n_frames : int
+        Total frames to keep in history when making prediction
+        should be the same as env board size
+    _buffer_size : int
+        Size of the buffer, how many examples to keep in memory
+        should be large for DQN
+    _n_actions : int
+        Total actions available in the env, should be same as env
+    _gamma : float
+        Reward discounting to use for future rewards, useful in policy
+        gradient, keep < 1 for convergence
+    _use_target_net : bool
+        If use a target network to calculate next state Q values,
+        necessary to stabilise DQN learning
+    _input_shape : tuple
+        Tuple to store individual state shapes
+    _board_grid : Numpy array
+        A square filled with values from 0 to board size **2,
+        Useful when converting between row, col and int representation
+    _version : str
+        model version string
+    """
+    def __init__(self, board_size=10, frames=2, buffer_size=10000,
+                 gamma=0.99, n_actions=3, use_target_net=True,
+                 version=''):
+        """ initialize the agent
+
+        Parameters
+        ----------
+        board_size : int, optional
+            The env board size, keep > 6
+        frames : int, optional
+            The env frame count to keep old frames in state
+        buffer_size : int, optional
+            Size of the buffer, keep large for DQN
+        gamma : float, optional
+            Agent's discount factor, keep < 1 for convergence
+        n_actions : int, optional
+            Count of actions available in env
+        use_target_net : bool, optional
+            Whether to use target network, necessary for DQN convergence
+        version : str, optional except NN based models
+            path to the model architecture json
+        """
+        self._board_size = board_size
+        self._n_frames = frames
+        self._buffer_size = buffer_size
+        self._n_actions = n_actions
+        self._gamma = gamma
+        self._use_target_net = use_target_net
+        self._input_shape = (self._board_size, self._board_size, self._n_frames)
+        # reset buffer also initializes the buffer
+        self.reset_buffer()
+        self._board_grid = np.arange(0, self._board_size**2)\
+                             .reshape(self._board_size, -1)
+        self._version = version
+
+    def get_gamma(self):
+        """Returns the agent's gamma value
+
+        Returns
+        -------
+        _gamma : float
+            Agent's gamma value
+        """
+        return self._gamma
+
+    def reset_buffer(self, buffer_size=None):
+        """Reset current buffer 
+        
+        Parameters
+        ----------
+        buffer_size : int, optional
+            Initialize the buffer with buffer_size, if not supplied,
+            use the original value
+        """
+        if(buffer_size is not None):
+            self._buffer_size = buffer_size
+        self._buffer = ReplayBufferNumpy(self._buffer_size, self._board_size, 
+                                    self._n_frames, self._n_actions)
+
+    def get_buffer_size(self):
+        """Get the current buffer size
+        
+        Returns
+        -------
+        buffer size : int
+            Current size of the buffer
+        """
+        return self._buffer.get_current_size()
+
+    def add_to_buffer(self, board, action, reward, next_board, done, legal_moves):
+        """Add current game step to the replay buffer
+
+        Parameters
+        ----------
+        board : Numpy array
+            Current state of the board, can contain multiple games
+        action : Numpy array or int
+            Action that was taken, can contain actions for multiple games
+        reward : Numpy array or int
+            Reward value(s) for the current action on current states
+        next_board : Numpy array
+            State obtained after executing action on current state
+        done : Numpy array or int
+            Binary indicator for game termination
+        legal_moves : Numpy array
+            Binary indicators for actions which are allowed at next states
+        """
+        self._buffer.add_to_buffer(board, action, reward, next_board, 
+                                   done, legal_moves)
+
+    def save_buffer(self, file_path='', iteration=None):
+        """Save the buffer to disk
+
+        Parameters
+        ----------
+        file_path : str, optional
+            The location to save the buffer at
+        iteration : int, optional
+            Iteration number to tag the file name with, if None, iteration is 0
+        """
+        if(iteration is not None):
+            assert isinstance(iteration, int), "iteration should be an integer"
+        else:
+            iteration = 0
+        with open("{}/buffer_{:04d}".format(file_path, iteration), 'wb') as f:
+            pickle.dump(self._buffer, f)
+
+    def load_buffer(self, file_path='', iteration=None):
+        """Load the buffer from disk
+        
+        Parameters
+        ----------
+        file_path : str, optional
+            Disk location to fetch the buffer from
+        iteration : int, optional
+            Iteration number to use in case the file has been tagged
+            with one, 0 if iteration is None
+
+        Raises
+        ------
+        FileNotFoundError
+            If the requested file could not be located on the disk
+        """
+        if(iteration is not None):
+            assert isinstance(iteration, int), "iteration should be an integer"
+        else:
+            iteration = 0
+        with open("{}/buffer_{:04d}".format(file_path, iteration), 'rb') as f:
+            self._buffer = pickle.load(f)
+
+    def _point_to_row_col(self, point):
+        """Covert a point value to row, col value
+        point value is the array index when it is flattened
+
+        Parameters
+        ----------
+        point : int
+            The point to convert
+
+        Returns
+        -------
+        (row, col) : tuple
+            Row and column values for the point
+        """
+        return (point//self._board_size, point%self._board_size)
+
+    def _row_col_to_point(self, row, col):
+        """Covert a (row, col) to value
+        point value is the array index when it is flattened
+
+        Parameters
+        ----------
+        row : int
+            The row number in array
+        col : int
+            The column number in array
+        Returns
+        -------
+        point : int
+            point value corresponding to the row and col values
+        """
+        return row*self._board_size + col
